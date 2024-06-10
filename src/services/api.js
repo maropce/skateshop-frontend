9fa4921f20b737b42a3a061bc5da69188131e450
maropce <47991391+maropce@users.noreply.github.com>
2024-06-10 09:36:09 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 const API_URL = 'https://skateshop-4qr5.onrender.com';
 
 export const registerUser = (user) => {
-  return axios.post(`${API_URL}/api/auth/register`, user);
+  return axios.post(`${API_URL}/register`, user);
 };
 
 export const loginUser = (credentials) => {
-  return axios.post(`${API_URL}/api/auth/login`, credentials);
+  return axios.post(`${API_URL}/login`, credentials);
 };
 
 export const fetchProducts = () => {
-  return axios.get(`${API_URL}/api/products`);
+  return axios.get(`${API_URL}/api/v1/products`);
 };
